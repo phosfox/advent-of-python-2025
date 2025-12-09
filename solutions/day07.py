066cff8967910de5533fe4daf6cf9a9e52ac871b
phosfox <mordredur@googlemail.com>
2025-12-09 01:03:22 +0100

    solve day7

--- solutions/day07.py
+++ solutions/day07.py
@@ -1,6 +1,8 @@
+from collections import Counter, defaultdict
 from pathlib import Path
 
 import numpy as np
+from numpy._core.numeric import argwhere
 from numpy._typing import NDArray
 
 
@@ -11,9 +13,12 @@ def parse(input) -> NDArray:
     return grid
 
 
-def part1():
-    grid = parse("inputs/day07_test.txt")
-    print(np.sum(grid == "^"))
+def parse2(input) -> list[str]:
+    lines = Path(input).read_text().strip().splitlines()
+    return lines
+
+
+def grid_with_paths(grid):
     width, height = grid.shape
     split = 0
     for (x, y), ele in np.ndenumerate(grid):
@@ -32,43 +37,35 @@ def part1():
                 grid[front_right] = "|"
         if ele == "|" and grid[front] != "^":
             grid[front] = "|"
+    return (grid, split)
 
+
+def part1():
+    print("part 1")
+    grid, split = grid_with_paths(parse("inputs/day07_test.txt"))
     print(grid)
-    print(np.argwhere(grid == "|"))
-    print("amount of |", np.sum(grid == "|"))
-    print("amount of ^", np.sum(grid == "^"))
     print(split)
     pass
+
+
+def delete_beam(beams: list, beam: int):
+    if beam in beams:
+        beams.remove(beam)
 
 
 def part2():
-    grid = parse("inputs/day07.txt")
-    splitters = np.sum(grid == "^")
-    width, height = grid.shape
-    split = 0
-    paths = set()
-    for (x, y), ele in np.ndenumerate(grid):
-        front = (x + 1, y)
-        front_left = (x + 1, y - 1)
-        front_right = (x + 1, y + 1)
-        if x == height or y == width - 1:
-            continue
-        if ele == "S":
-            grid[front] = "|"
-        if ele == "|" and grid[front] == "^":
-            if grid[front_left] == ".":
-                grid[front_left] = "|"
-                split += 1
-            if grid[front_right] == ".":
-                grid[front_right] = "|"
-                split += 1
-        if ele == "|" and grid[front] != "^":
-            grid[front] = "|"
-
-    available_paths = np.argwhere(grid == "|")
+    print("part 2")
+    grid, split = grid_with_paths(parse("inputs/day07_test.txt"))
     print(grid)
-    last_row_ways = np.sum(grid[height:] == "|")
-    print(split)
-    print(last_row_ways)
-    print(split + last_row_ways - 1)
-    pass
+    (x, y) = np.argwhere(grid == "S")[0]
+    bs = defaultdict(int)
+    bs[int(y)] = 1
+    for (x, y), ele in np.ndenumerate(grid):
+        if ele == "^" and grid[x - 1, y] == "|":
+            print(bs)
+            paths = bs[y]
+            bs[y] -= paths
+            bs[y-1] += paths
+            bs[y+1] += paths
+    print(sum(bs.values()))
+    print(bs.values())
